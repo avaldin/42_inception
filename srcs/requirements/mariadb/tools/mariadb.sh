d57e234262c2cf57f218774f76e1f0116f9e71f8
avaldin <valdinalan.pro@gmail.com>
2025-02-28 14:50:35 +0100

    go back on the 11 version

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "ssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+
 if [ -z "$SQL_DATABASE" ] || [ -z "$SQL_USER" ] || [ -z "$SQL_PASSWORD" ] || [ -z "$SQL_ROOT_PASSWORD" ]; then
   echo "One or more required environment variables are not set."
   echo "Please set SQL_DATABASE, SQL_USER, SQL_PASSWORD, and SQL_ROOT_PASSWORD."
@@ -8,7 +8,7 @@ fi
 
 chgrp -R mysql /var/lib/mysql
 chmod -R g+rwx /var/lib/mysql
-echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
 if [ -d "/var/lib/mysql/$SQL_DATABASE" ]; then
   echo "Database already exits"
 else
@@ -18,7 +18,7 @@ if [ ! -d "/var/lib/mysql" ]; then
 fi
 
 service mariadb start
-echo  "xcxxxxxcxxxxxxxoooofoooooond"
+
 mariadb << EOL
 CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
 CREATE USER IF NOT EXISTS $SQL_USER@'localhost' IDENTIFIED BY "$SQL_PASSWORD";
@@ -26,7 +26,7 @@ GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%' IDENTIFIED BY "$SQL_PAS
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD("$SQL_ROOT_PASSWORD");
 FLUSH PRIVILEGES;
 EOL
-echo "uuuuuuuuuuuuuuu"
+
 service mariadb stop
 fi
 exec "$@"
